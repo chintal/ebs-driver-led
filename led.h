d3895989c993669f381884902fb2e720aafdd3e0
Chintalagiri Shashank <shashank.chintalagiri@gmail.com>
2018-04-05 01:51:16 +0530

    Initial commit

---
+++ led.h
@@ -0,0 +1,28 @@
+
+
+#include "bsp/hal/uc/types.h"
+#include "bsp/hal/uc/gpio.h"
+
+
+static inline void led_init(PORTSELECTOR_t port,
+                            PINSELECTOR_t  pin){
+    gpio_conf_output(port, pin);
+}
+
+static inline void led_on(PORTSELECTOR_t port,
+                          PINSELECTOR_t  pin){
+    #if BOARD_LED_POLARITY == 1 
+        gpio_set_output_high(port, pin);
+    #else
+        gpio_set_output_low(port, pin);
+    #endif
+}
+
+static inline void led_off(PORTSELECTOR_t port,
+                           PINSELECTOR_t  pin){
+    #if BOARD_LED_POLARITY == 1 
+        gpio_set_output_low(port, pin);
+    #else
+        gpio_set_output_high(port, pin);
+    #endif
+}
